5038d6f4a7cf1b5c0c09655ccf8168d722323b9f
katherine <kathyhuaraca22@gmail.com>
2021-08-29 17:56:08 -0500

    Add Application

---
+++ videojuego/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+namespace videojuego.Data
+{
+    public class ApplicationDbContext:IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<videojuego.Models.Producto> DataContactos { get; set; } 
+    }
+}
